0f719adde60c44b97951b44164cf180777ea5ffb
davidh1832 <122473351+davidh1832@users.noreply.github.com>
2026-04-07 13:51:23 -0400

    Add files via upload

---
+++ TestMHCBApp.swift
@@ -0,0 +1,22 @@
+//
+//  TestMHCBApp.swift
+//  TestMHCB
+//
+//  Created by David Headen on 12/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TestMHCBApp: App {
+    // If isLoggedIn is true, the app will launch straight to the chatbot
+    @AppStorage("isLoggedIn") private var isLoggedIn: Bool = false
+
+    var body: some Scene {
+        WindowGroup {
+           
+                MentalHealthChatbotUI()
+                    .transition(.opacity) // Smooth transition when switching
+            }
+        }
+    }
